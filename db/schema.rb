66ab88a650bd32ab808700bd668a09cf89350e79
Manuel Ferrer <cmanuferrer@gmail.com>
2024-07-15 19:51:50 -0300

    revert fonts

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_07_10_025309) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
