05e4363e0b37c6bcab2b3b0baa8737387bb286d3
Carlos Manuel Ferrer <94634890+puzzle404@users.noreply.github.com>
2024-07-19 02:24:47 -0300

    Experiences controller, create action

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -4,11 +4,27 @@ class ExperiencesController < ApplicationController
   end
 
   def create
+    @experience = Experience.new(experience_params)
+    if @experience.save
+      redirect_to root_path, notice: 'Todo Ok'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def find_experience
+    @experience - Experience.find(params[:id])
+  end
+
+  def experience_params
+    params.require(:experience).permit(:id, :name, :description, :start_date, :end_date, :role, :created_at, :updated_at, :short_description, photos: [], skill_ids: [])
   end
 end
