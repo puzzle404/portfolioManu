158090d32f24a12346ac6b8d72e173b3f4db5b62
Manuel Ferrer <cmanuferrer@gmail.com>
2024-08-03 22:50:19 -0300

    avances en estilo de card de experiences

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,4 +2,5 @@ class Experience < ApplicationRecord
   has_many :experience_skills
   has_many :skills, through: :experience_skills
   has_many_attached :photos
+  validates :short_description, length: { maximum: 200 }
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
   has_many :project_skills
   has_many :skills, through: :project_skills
   has_many_attached :photos
+  validates :short_description, length: { maximum: 50 }
 end
