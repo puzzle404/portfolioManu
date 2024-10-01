f83d824a8ac51ed4f9c913428507e8dfb32ef9b7
Manuel Ferrer <cmanuferrer@gmail.com>
2024-10-01 17:45:20 -0300

    Dependent destroy project with skills, adn short desxcription maximun 100 characters

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,10 @@
 class Project < ApplicationRecord
   include Chunkable
-  has_many :project_skills
+  has_many :project_skills, dependent: :destroy
   has_many :skills, through: :project_skills
   has_many_attached :photos
   has_many :chunks, as: :chunkable, dependent: :destroy
-  validates :short_description, length: { maximum: 50 }
+  validates :short_description, length: { maximum: 100 }
 
   def chunkable_s
     project_content = [
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,5 +95,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  Rails.application.config.session_store :cookie_store, key: '_porfolio_manu_session', domain: :all, tld_length: 2
+  Rails.application.config.session_store :cookie_store, key: '_porfolio_manu_session', domain: :all, tld_length: 2,  expire_after: 30.days
 end
