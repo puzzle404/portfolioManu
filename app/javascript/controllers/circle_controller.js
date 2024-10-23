729e9ee2f015dd790d30b1d7946b7810372a87e8
Manuel Ferrer <cmanuferrer@gmail.com>
2024-10-23 12:07:11 -0300

    Refactorizacion html home page en partial

--- app/javascript/controllers/circle_controller.js
+++ app/javascript/controllers/circle_controller.js
@@ -2,16 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-        });
-      });
-    });
-
     this.circle = document.querySelector('.circle');
 
     document.addEventListener('mousemove', (e) => {
