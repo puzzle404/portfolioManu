6f01f0f443f3093b8b2bc74beb264fe8bb2ff22e
Manuel Ferrer <cmanuferrer@gmail.com>
2024-10-23 10:54:29 -0300

    Se corrige error de consola de navegador, referido a la reproduccion de youtube continua cuando cambiamos de paginas en la app

--- app/javascript/controllers/audio_controller.js
+++ app/javascript/controllers/audio_controller.js
@@ -1,11 +1,11 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["player"];
   player = null;
 
   connect() {
-    // Inserta el reproductor de YouTube si no existe
+    // Inserta el reproductor de YouTube si no existe ya
     if (!document.querySelector('#player')) {
       const template = this.element;
       document.body.before(template.content.cloneNode(true));
@@ -13,19 +13,26 @@ export default class extends Controller {
     }
 
     // Escuchar el evento personalizado del botón del navbar
-    document.addEventListener('togglePlayPause', this.playPause.bind(this));
+    if (!this.listenerAdded) {
+      document.addEventListener('togglePlayPause', this.playPause.bind(this));
+      this.listenerAdded = true;  // Evita añadir el listener más de una vez
+    }
   }
 
   loadYouTubeAPI() {
-    // Carga la API de YouTube
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    // Carga la API de YouTube solo si no se ha cargado ya
+    if (!window.YT) {
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
 
     // Espera a que la API de YouTube esté lista
     window.onYouTubeIframeAPIReady = () => {
-      this.initializePlayer();
+      if (!this.player) { // Solo inicializa si no hay un player existente
+        this.initializePlayer();
+      }
     };
   }
 
@@ -38,27 +45,25 @@ export default class extends Controller {
   }
 
   onPlayerReady() {
-    // Player is ready, but we don't need to do anything specific here
+    console.log('Player is ready');
   }
 
   playPause() {
-    const playerState = this.player.getPlayerState();
-    const musicIcon = document.querySelector(".music-icon");
-    const playIcon = document.querySelector(".play-icon");
-    const pauseIcon = document.querySelector(".pause-icon");
-
-    if (playerState === YT.PlayerState.PLAYING) {
-      this.player.pauseVideo();
-      musicIcon.style.display = "none";
-      playIcon.style.display = "block";
-      console.log('paused event')
-    } else {
-      console.log('playing event')
-
-
-      this.player.playVideo();
-      playIcon.style.display = "none";
-      musicIcon.style.display = "block";
+    if (this.player) {
+      const playerState = this.player.getPlayerState();
+      const musicIcon = document.querySelector(".music-icon");
+      const playIcon = document.querySelector(".play-icon");
+      if (playerState === YT.PlayerState.PLAYING) {
+        this.player.pauseVideo();
+        musicIcon.style.display = "none";
+        playIcon.style.display = "block";
+        console.log('paused event');
+      } else {
+        this.player.playVideo();
+        playIcon.style.display = "none";
+        musicIcon.style.display = "block";
+        console.log('playing event');
+      }
     }
   }
 }
