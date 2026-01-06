e26f346afb62eca9bf2b05a1a38d8b921f68b46f
Carlos Manuel Ferrer <94634890+puzzle404@users.noreply.github.com>
2026-01-06 11:08:47 -0300

    prod cookies stup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,9 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  Rails.application.config.session_store :cookie_store, key: '_porfolio_manu_session', domain: :all, tld_length: 2,  expire_after: 30.days
+  Rails.application.config.session_store :cookie_store,
+  key: '_porfolio_manu_session',
+  expire_after: 30.days
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
