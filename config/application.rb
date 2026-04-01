225dc85865bba5d831db0d521110a498119a81f0
Manuel Ferrer <cmanuferrer@gmail.com>
2026-04-01 01:17:12 -0300

    timezone

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module PortfolioManu
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Buenos Aires"
     # config.eager_load_paths << Rails.root.join("extras")
     # Para manjejar jobs
     config.active_job.queue_adapter = :solid_queue
